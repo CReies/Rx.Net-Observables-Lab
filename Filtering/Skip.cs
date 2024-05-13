f5b362d0a35f9227bf0002637fab0e477319579d
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-13 05:07:53 -0500

    Add Skip class with example usage

---
+++ Filtering/Skip.cs
@@ -0,0 +1,22 @@
+﻿using System.Reactive.Linq;
+
+namespace Observables.Lab.Filtering;
+
+public class Skip
+{
+	public static void Exec()
+	{
+		var source = Observable.Range( 1, 10 );
+
+		/* Skips the first n events */
+		var skip = source.Skip( 5 );
+
+		skip.Subscribe(
+			Console.WriteLine,
+			error => Console.WriteLine( $"Error: {error}" ),
+			() => Console.WriteLine( "Observable sequence completed" )
+		);
+
+		Console.ReadLine();
+	}
+}
