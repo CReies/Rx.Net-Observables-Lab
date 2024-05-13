cad7ae3fd2a41622eae2bf17ae37a98d6e7d53a1
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-13 00:43:12 -0500

    Add Distinct method example

---
+++ Filtering/Distinct.cs
@@ -0,0 +1,23 @@
+﻿using System.Reactive.Linq;
+
+namespace Observables.Lab.Filtering;
+
+public class Distinct
+{
+	public static void Exec()
+	{
+		List<int> numbersList = [1, 1, 2, 2, 2, 3, 4, 5, 5];
+		var source = numbersList.ToObservable();
+
+		/* Filters if there are event values repeated */
+		var distinct = source.Distinct();
+
+		distinct.Subscribe(
+			Console.WriteLine,
+			error => Console.WriteLine( $"Error: {error}" ),
+			() => Console.WriteLine( "Observable sequence completed" )
+		);
+
+		Console.ReadLine();
+	}
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ public class Program
 {
 	static void Main( string[] args )
 	{
-		Throttle.Exec();
-
+		Distinct.Exec();
 	}
 }
