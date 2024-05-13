e2dde7b47c3aa24cf0c98336738669eb016691fe
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-13 00:28:37 -0500

    Add Throttle method example

---
+++ Filtering/Throttle.cs
@@ -0,0 +1,21 @@
+﻿using System.Reactive.Linq;
+
+namespace Observables.Lab.Filtering;
+
+public class Throttle
+{
+	public static void Exec()
+	{
+		var source = Observable.Interval(TimeSpan.FromMilliseconds(250));
+
+		var throttled = source.Throttle(TimeSpan.FromMilliseconds(900));
+
+		throttled.Subscribe(
+			Console.WriteLine,
+			error => Console.WriteLine($"Error: {error}"),
+			() => Console.WriteLine("Observable sequence completed")
+		);
+
+		Console.ReadLine();
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Observables.Lab.Filtering;
+
+namespace Observables.Lab;
+
+public class Program
+{
+	static void Main( string[] args )
+	{
+		Throttle.Exec();
+
+	}
+}
