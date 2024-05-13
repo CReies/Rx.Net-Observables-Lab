449f5e54b7f6b02d6eb54203d8cd577d64c9c37f
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-13 05:13:08 -0500

    Add Take class with example usage

---
+++ Filtering/Take.cs
@@ -0,0 +1,20 @@
+﻿namespace Observables.Lab.Filtering;
+
+public class Take
+{
+	public static void Exec()
+	{
+		var source = Observable.Range( 1, 10 );
+
+		/* Takes the first n events */
+		var take = source.Take( 5 );
+
+		take.Subscribe(
+			Console.WriteLine,
+			error => Console.WriteLine( $"Error: {error}" ),
+			() => Console.WriteLine( "Observable sequence completed" )
+		);
+
+		Console.ReadLine();
+	}
+}
