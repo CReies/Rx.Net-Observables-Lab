e8e376d2a7b904d47e45a40e8a4b01575d4fedbf
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-13 00:31:34 -0500

    Add description to the method

--- Filtering/Throttle.cs
+++ Filtering/Throttle.cs
@@ -6,14 +6,15 @@ public class Throttle
 {
 	public static void Exec()
 	{
-		var source = Observable.Interval(TimeSpan.FromMilliseconds(250));
+		var source = Observable.Interval( TimeSpan.FromMilliseconds( 250 ) );
 
-		var throttled = source.Throttle(TimeSpan.FromMilliseconds(900));
+		/* Filters the events if te time between them are less than the established */
+		var throttled = source.Throttle( TimeSpan.FromMilliseconds( 900 ) );
 
 		throttled.Subscribe(
 			Console.WriteLine,
-			error => Console.WriteLine($"Error: {error}"),
-			() => Console.WriteLine("Observable sequence completed")
+			error => Console.WriteLine( $"Error: {error}" ),
+			() => Console.WriteLine( "Observable sequence completed" )
 		);
 
 		Console.ReadLine();
