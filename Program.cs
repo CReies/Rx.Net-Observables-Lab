902765605edb956d39ac23f407187bce44c5bd64
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-13 19:52:40 -0500

    Add SelectMany method example

--- Program.cs
+++ Program.cs
@@ -7,6 +7,6 @@ public class Program
 {
 	static void Main( string[] args )
 	{
-		ManySelect.Exec();
+		SelectMany.Exec();
 	}
 }
---
+++ Transforming/SelectMany.cs
@@ -0,0 +1,21 @@
+﻿namespace Observables.Lab.Transforming;
+
+public class SelectMany
+{
+	public static void Exec()
+	{
+		List<string> fruits = ["apple", "orange", "watermelon"];
+		var source = fruits.ToObservable();
+
+		/* Emits the events that were emitted by the inner observable */
+		var selectMany = source.SelectMany( x => new[] { $"{x} genetically modified" }.ToObservable() );
+
+		selectMany.Subscribe(
+			x => Console.WriteLine( $"Emitted: {x}" ),
+			error => Console.WriteLine( $"Error: {error}" ),
+			() => Console.WriteLine( "Observable sequence completed" )
+		);
+
+		Console.ReadLine();
+	}
+}
