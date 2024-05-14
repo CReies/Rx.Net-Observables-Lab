bdea039fd0fbc690cbd088081f2e926a4d4780da
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-14 00:16:54 -0500

    Add GroupJoin method example

---
+++ Combining/GroupJoin.cs
@@ -0,0 +1,22 @@
+﻿namespace Observables.Lab.Combining;
+
+public class GroupJoin
+{
+	public static void Exec()
+	{
+		var source1 = Observable.Interval( TimeSpan.FromSeconds( 1 ) ).Select( x => $"Source 1: {x}" );
+		var source2 = Observable.Interval( TimeSpan.FromSeconds( 2 ) ).Select( x => $"Source 2: {x}" );
+
+		/* Similar to Join, but groups the pairs of events in another internal observable  */
+		var combined = source1.GroupJoin(
+			source2,
+			_ => Observable.Timer( TimeSpan.FromSeconds( 3 ) ),
+			_ => Observable.Timer( TimeSpan.FromSeconds( 3 ) ),
+			( s1, s2 ) => s2.Select( x => $"{s1} - {x}" )
+		);
+
+		combined.Merge().Subscribe( Console.WriteLine );
+
+		Console.ReadLine();
+	}
+}
